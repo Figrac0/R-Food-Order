175c6fa2800ea4502574483ebcb5029887e33791
sergey <serjjiniuss@gmail.com>
2025-10-21 15:54:19 +0400

    MVP

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,31 @@
-import { useContext } from "react";
+import { useContext, useActionState } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
 import { currentFormatter } from "../util/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+};
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
+
+    const {
+        data,
+        // isLoading: isSending,
+        error,
+        sendRequest,
+        clearData,
+    } = useHttp("http://localhost:3000/orders", requestConfig);
 
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
         return totalPrice + item.quantity * item.price;
@@ -18,31 +35,83 @@ export default function Checkout() {
         userProgressCtx.hideCheckout();
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
+    function handleFinish() {
+        userProgressCtx.hideCheckout();
+        cartCtx.clearCart();
+        clearData();
+    }
 
-        const fd = new FormData(event.target);
+    // function handleSubmit(event) {
+    //     event.preventDefault();
+
+    //     const fd = new FormData(event.target);
+    //     const customerData = Object.fromEntries(fd.entries());
+
+    //     sendRequest(
+    //         JSON.stringify({
+    //             order: {
+    //                 items: cartCtx.items,
+    //                 customer: customerData,
+    //             },
+    //         })
+    //     );
+    // }
+
+    async function checkoutAction(prevState, fd) {
         const customerData = Object.fromEntries(fd.entries());
 
-        fetch("http://localhost:3000/orders", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
+        await sendRequest(
+            JSON.stringify({
                 order: {
                     items: cartCtx.items,
                     customer: customerData,
                 },
-            }),
-        });
+            })
+        );
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const [formState, formAction, isSending] = useActionState(
+        checkoutAction,
+        null
+    );
+
+    let actions = (
+        <>
+            <Button type="button" textOnly onClick={handleClose}>
+                Close
+            </Button>
+            <Button>Submit Order</Button>
+        </>
+    );
+
+    if (isSending) {
+        actions = <span>Sending order data...</span>;
+    }
+
+    if (data && !error) {
+        return (
+            <Modal
+                open={userProgressCtx.progress === "checkout"}
+                onClose={handleFinish}>
+                <h2>Success!</h2>
+                <p>Your order was submitted successfully.</p>
+                <p>
+                    We will get back to you with more details via email within
+                    the nest few minutes
+                </p>
+                <p className="modal-action">
+                    <Button onClick={handleFinish}>Okay</Button>
+                </p>
+            </Modal>
+        );
     }
 
     return (
         <Modal
             open={userProgressCtx.progress === "checkout"}
             onClose={handleClose}>
-            <form onSubmit={handleSubmit}>
+            <form action={formAction}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currentFormatter.format(cartTotal)}</p>
                 <Input label="Full Name" type="text" id="name" />
@@ -52,12 +121,11 @@ export default function Checkout() {
                     <Input label="Postal Code" type="text" id="postal-code" />
                     <Input label="City" type="text" id="city" />
                 </div>
-                <p className="modal-actions">
-                    <Button type="button" textOnly onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button>Submit Order</Button>
-                </p>
+
+                {error && (
+                    <Error title="Failed to submit order" message={error} />
+                )}
+                <p className="modal-actions">{actions}</p>
             </form>
         </Modal>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,33 @@
 import { useContext } from "react";
+
+import Button from "./UI/Button.jsx";
 import logoImg from "../assets/logo.jpg";
-import Button from "./UI/Button";
-import CartContext from "../store/CartContext";
-import UserProgressContext from "../store/UserProgressContext";
+import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
 
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
     function handleShowCart() {
         userProgressCtx.showCart();
     }
-
-    const totalCartItems = cartCtx.items.reduce(
-        (totalNumberOfNumbers, item) => {
-            return totalNumberOfNumbers + item.quantity;
-        },
-        0
-    );
 
     return (
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt="A restaurant" />
                 <h1>ReactFood</h1>
-                <nav>
-                    <Button textOnly onClick={handleShowCart}>
-                        Cart ({totalCartItems})
-                    </Button>
-                </nav>
             </div>
+            <nav>
+                <Button textOnly onClick={handleShowCart}>
+                    Cart ({totalCartItems})
+                </Button>
+            </nav>
         </header>
     );
 }
